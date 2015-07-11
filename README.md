a53144b9f9cb0fbcb6b366725e0fff83b06e9d44
文红军 <wenhongjun@coolhey.cn>
2015-07-11 17:17:46 +0800

    auto add version

--- README.md
+++ README.md
@@ -5,13 +5,22 @@
 ```js
 var apidocHistory = require("gulp-apidoc-history");
 var concat = require("gulp-concat");
+var apidoc = require('gulp-apidoc');
 var fs = require("fs");
 
-gulp.task('apidoc:history', function () {
+gulp.task('apidoc', function () {
     var apidocJson = JSON.parse(fs.readFileSync("apidoc.json"));
+
     gulp.src(["api/**/*.js", "!api/apidocs/*.js"])
-        .pipe(apidocHistory())
+        .pipe(apidocHistory({apidoc: apidocJson}))
         .pipe(concat(apidocJson.version + "_apidoc.js"))
         .pipe(gulp.dest('api/apidocs'));
+
+    apidoc.exec({
+        src: "api/apidocs/",
+        dest: buildDirectories.dest + "/apidoc",
+        debug: false,
+        includeFilters: [ ".*\\.js$" ]
+    });
 });
 ```
--- index.js
+++ index.js
@@ -9,6 +9,14 @@ var PLUGIN_NAME = 'gulp-apidoc-history';
 module.exports = function (options) {
     options = options || {};
 
+    if (!options.apidoc) {
+        throw new PluginError(PLUGIN_NAME, 'Need apidoc option');
+    }
+
+    if (!options.apidoc.version) {
+        throw new PluginError(PLUGIN_NAME, 'Please set version in your apidoc.json');
+    }
+
     return through.obj(function (file, encode, cb) {
         if (file.isNull()) {
             return cb(null, file);
@@ -20,6 +28,12 @@ module.exports = function (options) {
 
         if (file.isBuffer()) {
             var content = file.contents.toString().match(/\/\*\*(.|\n|\r|\r\n)*?@api(.|\n|\r|\r\n)*?\*\//gm) || [];
+
+            for (var i = 0, length = content.length; i < length; i++) {
+                var block = content[i];
+                content[i] = block.replace(/^\/\*\*/, "/**\n     * @apiVersion " + options.apidoc.version);
+            }
+
             file.contents = new Buffer(content.join("\n\n"));
         }
 
